e0f1d16230a0fb0aa8a1373f349bec3d9affd109
goofy <goofy@rickroll.com>
2024-10-12 15:49:28 +0530

    Chore: Add part files

--- push.sh
+++ push.sh
@@ -6,9 +6,9 @@ set -ex
 git config --global user.name "goofy"
 git config --global user.email "goofy@rickroll.com"
 
-for i in {1..9}
-do
-    git add */part$i* || continue
-    git commit -m "Chore: Add part$i files" || continue
-    git push || continue
-done
+files=$(git ls-files --others --exclude-standard | head -10)
+if [ -n "$files" ]; then
+    git add $files
+    git commit -m "Chore: Add part files"
+    git push
+fi
